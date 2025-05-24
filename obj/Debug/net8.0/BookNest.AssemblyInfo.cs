2b5780204b7f3d54a0c62516898b38c371a1b07a
1307Bao <7713bbbo@gmail.com>
2025-05-25 02:00:24 +0700

    Update UI of About Page and Footer + Header

--- obj/Debug/net8.0/BookNest.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookNest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab029939fb2de1bde0c2a3be134f832b0ca1556")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141054d64ba60401a91a5337df7d2cb9863b3e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
