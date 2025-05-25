955cdf0ec930e1f133f40b4c5b1ab6781f912e42
1307Bao <7713bbbo@gmail.com>
2025-05-25 17:25:43 +0700

    Update UI mainpage, books page and book detail

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookNest.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+
+}
--- obj/Debug/net8.0/BookNest.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookNest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141054d64ba60401a91a5337df7d2cb9863b3e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076a0b238c1de00bb399585a31465e1c8bc9b9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
