f9a7ea268c69befacdcef6adacde5381a3d7dc34
Nguyen Thai Bao <7713bbbo@gmail.com>
2025-07-31 18:27:56 +0700

    update readme

--- obj/Debug/net8.0/BookNest.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookNest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fec1a0ee3a3d2188539e960d1ee34a3bd08c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc5fd76ebaf75edc5c04e0c7cbefaf6afbee406")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
